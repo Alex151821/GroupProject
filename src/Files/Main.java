45b3d67d96c91a8511f4eb2a2071be6753de3df2
Dennis Drew <dennisadrew@icloud.com>
2020-01-29 13:25:31 -0700

    add misc.xml to .gitignore

--- src/Files/Main.java
+++ src/Files/Main.java
@@ -3,9 +3,7 @@ package Files;
 public class Main {
     public static void main(String[] args){
         Files.Stuff stuff = new Files.Stuff();
-
         stuff.hi();
-
+        stuff.print("hi");
     }
-
 }
