a275d7b5c7b3ec1641e2d7e6d2b41c033753b8e8
Alex151821 <dennisadrew@icloud.com>
2020-01-29 21:00:25 +0000

    added board building functionality;

--- src/Files/Main.java
+++ src/Files/Main.java
@@ -2,11 +2,20 @@ package Files;
 
 public class Main {
     public static void main(String[] args){
-        int [][] board = new int[8][8];
-        for (int x: board){
+        int [][] board = init();
+    }
 
+    public static int[][] init(){
+         int [][] board = new int[8][8];
+        int [] board0 = {0, 1, 0, 1, 0, 1, 0, 1};
+        int [] board1 = {1, 0, 1, 0, 1, 0, 1, 0};
+        for (int i = 0; i < board.length; i++){
+            if (i % 2 == 0){
+                board[i] = board0;
+            } else{
+                board[i] = board1;
+            }
         }
-
-        
+        return board;
     }
 }
