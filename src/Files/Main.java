e3b8d84619e99c2d38b9dcc4b1664fe22e4af123
Dennis Drew <dennisadrew@icloud.com>
2020-01-31 13:09:43 -0700

    things

--- src/Files/Main.java
+++ src/Files/Main.java
@@ -3,13 +3,13 @@ import java.io.Serializable;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Files.Checkers checkers = new Files.Checkers();
         Files.Stuff stuff = new Files.Stuff();
         Scanner scan = new Scanner(System.in);
         //Integer [][] board = init();
         // I also made an init(), you can also do this:
-        Integer [][] board2 = checkers.initBoard(8);
+        Integer[][] board2 = checkers.initBoard(8);
         //checkers.printBoard(board);
         checkers.printBoard(board2);
         stuff.print("Enter form x");
@@ -30,6 +30,7 @@ public class Main {
 
     }
 
+    /*
     //unnecessary
     public static Integer[][] init(){
         Integer [][] board = new Integer[8][8];
@@ -44,4 +45,5 @@ public class Main {
         }
         return board;
     }
+*/
 }
