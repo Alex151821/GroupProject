e089610f54790604386f87f77d670f40b2814139
Dennis Drew <dennisadrew@icloud.com>
2020-01-31 12:59:50 -0700

    added board move test code

--- src/Files/Main.java
+++ src/Files/Main.java
@@ -1,13 +1,33 @@
 package Files;
+import java.io.Serializable;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         Files.Checkers checkers = new Files.Checkers();
+        Files.Stuff stuff = new Files.Stuff();
+        Scanner scan = new Scanner(System.in);
         //Integer [][] board = init();
         // I also made an init(), you can also do this:
         Integer [][] board2 = checkers.initBoard(8);
         //checkers.printBoard(board);
         checkers.printBoard(board2);
+        stuff.print("Enter form x");
+
+        stuff.print("Enter form y");
+        stuff.print("Enter to x");
+        stuff.print("Enter to y");
+
+        Serializable[] out = checkers.movePiece(board2, 0, 2, 1, 3, 2);
+        board2 = (Integer[][]) out[0];
+        stuff.print((String) out[1]);
+        checkers.printBoard(board2);
+        out = checkers.movePiece(board2, 1, 3, 2, 2, 2);
+        board2 = (Integer[][]) out[0];
+        stuff.print((String) out[1]);
+        checkers.printBoard(board2);
+        scan.close();
+
     }
 
     //unnecessary
