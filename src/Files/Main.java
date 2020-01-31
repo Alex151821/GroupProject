b19bdfaea726c50d51bce6de157ee77a2075dc39
Alex151821 <dennisadrew@icloud.com>
2020-01-30 18:14:25 -0700

    a

--- src/Files/Main.java
+++ src/Files/Main.java
@@ -2,14 +2,16 @@ package Files;
 
 public class Main {
     public static void main(String[] args){
-        int [][] board = init();
+        Integer [][] board = init();
+        Files.Checkers checkers = new Files.Checkers();
+        checkers.printBoard(board);
     }
 
-    public static int[][] init(){
-         int [][] board = new int[8][8];
-        int [] board0 = {0, 1, 0, 1, 0, 1, 0, 1};
-        int [] board1 = {1, 0, 1, 0, 1, 0, 1, 0};
-        for (int i = 0; i < board.length; i++){
+    public static Integer[][] init(){
+        Integer [][] board = new Integer[8][8];
+        Integer [] board0 = {0, 1, 0, 1, 0, 1, 0, 1};
+        Integer [] board1 = {1, 0, 1, 0, 1, 0, 1, 0};
+        for (Integer i = 0; i < board.length; i++){
             if (i % 2 == 0){
                 board[i] = board0;
             } else{
